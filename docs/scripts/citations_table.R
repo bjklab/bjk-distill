086b70c1b92ce20984d519333eed5f94ec2fede6
Brendan Kelly <brendank@upenn.edu>
2021-02-01 16:27:55 -0500

    Add publications and projects pages.

---
+++ docs/scripts/citations_table.R
@@ -0,0 +1,34 @@
+#' #################################
+#' load libraries and set seed
+#' #################################
+library(tidyverse)
+#library(bibtex)
+library(gt)
+
+
+
+#' #################################
+#'  read and format citation data
+#' #################################
+
+read_lines(file = "./bib/bjk_bib.txt") %>%
+  enframe(name = "citation_index", value = "citation") %>%
+  filter(citation != "") %>%
+  select(-citation_index) %>%
+  separate(col = citation, into = c("authors","title","journal","date_vol_page","link"), sep = "\\. ") %>%
+  mutate(link = gsub("Available from\\: ","",link)) %>%
+  separate(link, into = "link", sep = " ", remove = TRUE, extra = "drop") %>%
+  filter(grepl("reply",title) == FALSE) %>%
+  select(-date_vol_page) %>%
+  mutate(
+    link = glue::glue("<a href='{link}'>{link}</a>"),
+    link = map(link, gt::html)) %>%
+  gt() %>%
+  gt::cols_label(authors = "Authors", title = "Title", journal = "Journal", link = "Link") %>%
+  gt::tab_options(table_body.hlines.color = "black",
+                  column_labels.border.bottom.color = "black",
+                  column_labels.border.top.color = "black",
+                  table_body.border.bottom.color = "black")
+
+
+
